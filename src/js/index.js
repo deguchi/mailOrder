1b3aed89608dac0786f10ddc1987004bef060e0d
deguchi <deguchik@gmail.com>
2021-03-26 10:50:51 +0900

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,3 @@
-console.log('Hello LooselyTemplate!')
-
 function getQueryString() {
     const params = {}
     location.search.substr(1).split('&').map(function(param) {
@@ -7,6 +5,24 @@ function getQueryString() {
         params[pairs[0]] = decodeURIComponent(pairs[1]);
     });
     return params;
+}
+const buildQueryString = (params) => {
+	let parts = [];
+	let add = (key, value) => {
+		parts.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
+	}
+	for (let key in params) {
+   		let param = params[key];
+   		if (Array.isArray(param)) {
+            param.map((value) => {
+   				add(key, value);
+   			});
+   		}
+   		else {
+   			add(key, param);
+   		}
+	}
+	return parts.join('&').replace(/%20/g, '+');
 }
 
 // title
@@ -61,5 +77,44 @@ if (params.title) {
         }
         // 入力時にメッセージが出てしまう問題対策
         e.target.oninput = (e) => e.target.setCustomValidity('')
-    })    
+    })
+    input.addEventListener('change', (e) => {
+        formData[e.target.name] = e.target.value
+        localStorage.setItem('mailOrderFormData', JSON.stringify(formData))
+    })
+})
+
+
+let formData = {}
+if (localStorage.getItem('mailOrderFormData')) {
+    formData = JSON.parse(localStorage.getItem('mailOrderFormData'))
+    Object.keys(formData).map((key) => {
+        document.querySelector(`input[name="${key}"]`).value = formData[key]
+    })
+}
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const subject = `[アプリ注文] ${params.title}`;
+    const shopName = 'ホホホ座'
+    const url = `http://hohoho-sv2.calil.jp/wp-admin/admin.php?page=order%26` + buildQueryString(params).replace(/&/g, '%26')
+    const orderUrl = `https://honto.jp/netstore/search_10${params.isbn}.html?srchf=1&tbty=1`.replace(/&/g, '%26');
+    let body = `${shopName} 様
+
+以下の注文をお願いします。
+
+書名: ${params.title}
+著者: ${params.author}
+出版社: ${params.publisher}
+ISBN: ${params.isbn}
+
+注文者: ${formData.name}
+電話番号: ${formData.tel}
+メールアドレス: ${formData.email}
+
+honto: ${orderUrl}
+
+管理用: ${url}`;
+    body = body.replace(/\n/g, '%0D%0A')
+    window.open(`mailto:${formData.mailto}?&body=${body}&subject=${subject}`)
 })
